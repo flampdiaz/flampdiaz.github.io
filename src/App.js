20edc21d21057e01464394466003efb036848a6a
FlamperDM <flampeyeiri12@gmail.com>
2020-09-01 21:10:07 -0400

    adding and adapting Agency bootstrap template to ReactJS

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
+
+import { portfolioLinks } from './util'
+
+import NavBar from './asset/components/NavBar';
+import Header from './asset/components/Header';
+import Services from './asset/components/Services';
+import Portfolio from './asset/components/Portfolio';
+import About from './asset/components/About';
+import OurTeam from './asset/components/OurTeam';
+import Affiliates from './asset/components/Affiliates';
+import ContactUs from './asset/components/ContactUs';
+import Footer from './asset/components/Footer';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div classNameName="App">
+      <NavBar></NavBar>
+      <Header></Header>
+      <Services></Services>
+      <Portfolio portfolioLinks={portfolioLinks}></Portfolio>
+      <About></About>
+      <OurTeam></OurTeam>
+      <Affiliates></Affiliates>
+      <ContactUs></ContactUs>
+      <Footer></Footer>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/util/index.js
@@ -0,0 +1,26 @@
+export const portfolioLinks = [
+  {
+    title: 'Threads',
+    caption: 'Illustration'
+  },
+  {
+    title: 'Explore',
+    caption: 'Graphic Design'
+  },
+  {
+    title: 'Finish',
+    caption: 'Identity'
+  },
+  {
+    title: 'Lines',
+    caption: 'Branding'
+  },
+  {
+    title: 'Southwest',
+    caption: 'Website Design'
+  },
+  {
+    title: 'Window',
+    caption: 'Photography'
+  }
+];
